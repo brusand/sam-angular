485312188f149d28706c3aac051b091732b1aaab
ebdu6461 <bruno.darrigues@orange.com>
2016-03-01 16:31:27 +0100

    del proxys

--- src/app/sam/sam.action.ts
+++ src/app/sam/sam.action.ts
@@ -1,12 +1,12 @@
 /// <reference path="sam.model.ts"/>;
 export module Sam {
     export class SamAction {
-        constructor(public model: any) {
+        constructor(public model) {
         }
         
         public init(data, present) {
             present = present || this.model.present ;
-            return present(data) ;
+            return present(data);
         }
     }
   
